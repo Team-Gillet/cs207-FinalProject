df8e473aa43b3f90809f8e5e001deaf8ad9a72cf
Lucie Gillet <luciegillet@hsph.harvard.edu>
2019-12-08 17:01:38 -0500

    rename autodiff reverse object and implement cos and sin

--- superautodiff/autodiffreverse.py
+++ superautodiff/autodiffreverse.py
@@ -5,8 +5,8 @@ index = 0
 forward_pass = pd.DataFrame(columns=['Node', 'd1', 'd1value', 'd2', 'd2value'])
 
 
-class AutoDiff():
-    """Creates an object for autodifferentiation
+class AutoDiffReverseReverse():
+    """Creates an object for AutoDiffReverseerentiation
 
     ATTRIBUTES
     ==========
@@ -16,7 +16,7 @@ class AutoDiff():
 
     EXAMPLES
     ========
-    >>> x = AutoDiff("x",4)
+    >>> x = AutoDiffReverse("x",4)
     >>> x.var
     {"x"}
     >>> x.val
@@ -57,73 +57,73 @@ class AutoDiff():
             forward_pass = forward_pass.append(row)
 
     def __add__(self, other):
-        """Performs addition on two AutoDiff objects"""
+        """Performs addition on two AutoDiffReverse objects"""
         try:
             total = {self.var: 1, other.var: 1}
-            return AutoDiff(self.val + other.val, None, der=total)
+            return AutoDiffReverse(self.val + other.val, None, der=total)
         except AttributeError:
-            return AutoDiff(self.val + other, None, {self.var: 1})
+            return AutoDiffReverse(self.val + other, None, {self.var: 1})
 
     def __radd__(self, other):
-        """Performs addition on two AutoDiff objects"""
+        """Performs addition on two AutoDiffReverse objects"""
         return self.__add__(other)
 
     def __sub__(self, other):
-        """Performs subtraction on two AutoDiff objects"""
+        """Performs subtraction on two AutoDiffReverse objects"""
         try:
             total = {self.var: 1, other.var: -1}
-            return AutoDiff(self.val - other.val, None, der=total)
+            return AutoDiffReverse(self.val - other.val, None, der=total)
         except AttributeError:
-            return AutoDiff(self.val - other, None, {self.var: 1})
+            return AutoDiffReverse(self.val - other, None, {self.var: 1})
 
     def __rsub__(self, other):
-        """Performs subtraction on two AutoDiff objects"""
+        """Performs subtraction on two AutoDiffReverse objects"""
         try:
             total = {self.var: 1, other.var: -1}
-            return AutoDiff(self.val - other.val, None, der=total)
+            return AutoDiffReverse(self.val - other.val, None, der=total)
         except AttributeError:
-            return AutoDiff(self.val - other, None, {self.var: -1})
+            return AutoDiffReverse(self.val - other, None, {self.var: -1})
 
     def __mul__(self, other):
-        """Performs subtraction on two AutoDiff objects"""
+        """Performs subtraction on two AutoDiffReverse objects"""
         try:
             total = {self.var: other.val, other.var: self.val}
-            return AutoDiff(self.val * other.val, None, total)
+            return AutoDiffReverse(self.val * other.val, None, total)
         except AttributeError:
-            return AutoDiff(self.val * other, None, {self.var: other})
+            return AutoDiffReverse(self.val * other, None, {self.var: other})
 
     def __rmul__(self, other):
-        """Performs subtraction on two AutoDiff objects"""
+        """Performs subtraction on two AutoDiffReverse objects"""
         return self.__mul__(other)
 
     def __neg__(self):
-        """Returns the negation of an AutoDiff object"""
+        """Returns the negation of an AutoDiffReverse object"""
         return 0 - self
 
     def __pow__(self, power):
-        """Performs exponentiation of an AutoDiff object with scalars values e.g x**3 """
+        """Performs exponentiation of an AutoDiffReverse object with scalars values e.g x**3 """
         value = power * (self.val) ** (power - 1)
         der = {k: value * v for k, v in self.der.items()}
-        return AutoDiff(self.val ** power, None, der)
+        return AutoDiffReverse(self.val ** power, None, der)
 
     def __rpow__(self, power):
-        """Performs exponentiation of an AutoDiff object with scalars values e.g. 3**x"""
+        """Performs exponentiation of an AutoDiffReverse object with scalars values e.g. 3**x"""
         value = power ** self.val
         der = {k: value * v * np.log(power) for k, v in self.der.items()}
-        return AutoDiff(value, None, der)
+        return AutoDiffReverse(value, None, der)
 
     def __truediv__(self, other):
-        """Performs division of an AutoDiff object with scalars and other AutoDiff objects"""
+        """Performs division of an AutoDiffReverse object with scalars and other AutoDiffReverse objects"""
         try:
             value = -1 / (other.val * other.val)
             total = {self.var: 1 / other.val, other.var: value * self.val}
-            return AutoDiff(self.val / other.val, None, total)
+            return AutoDiffReverse(self.val / other.val, None, total)
         except AttributeError:
             total = {self.var: 1 / other}
-            return AutoDiff(self.val / other, None, total)
+            return AutoDiffReverse(self.val / other, None, total)
 
     def __rtruediv__(self, other):
-        """Performs division of an AutoDiff object with scalars and other AutoDiff objects"""
+        """Performs division of an AutoDiffReverse object with scalars and other AutoDiffReverse objects"""
         value = -1 / (self.val * self.val)
         total = {self.var: other * value}
-        return AutoDiff(other / self.val, None, total)
+        return AutoDiffReverse(other / self.val, None, total)
