f41af278daa3570af54a2363441b87d62fc61e15
Sakari Jukarainen <jukarainen.sakari@gmail.com>
2019-12-09 19:11:38 -0500

    created tests for autodiffereverse

--- superautodiff/__init__.py
+++ superautodiff/__init__.py
@@ -1,4 +1,5 @@
 from superautodiff.autodiff import AutoDiff
 from superautodiff.autodiff import AutoDiffVector
 from superautodiff.autodiff import vectorize
+from superautodiff.autodiffreverse import AutoDiffReverse
 from superautodiff.functions import *
---
+++ tests/test_autodiffreverse.py
@@ -0,0 +1,114 @@
+import sys
+from collections import Counter
+import numpy as np
+import pytest
+import math
+
+sys.path.append('..')
+import superautodiff as sad
+
+
+def test_reverse_add():
+	x1 = sad.AutoDiffReverse(4, 'x1')
+	x2 = sad.AutoDiffReverse(3, 'x2')
+	
+	f = x1 + 1
+	assert f.der['x1'] ==  pytest.approx(1.0)
+	#assert f.val == pytest.approx(5.0)
+
+	f = 1 + x1
+	assert f.der['x1'] ==  pytest.approx(1.0)
+	#assert f.val == pytest.approx(5.0)
+
+	f = x1 + x2
+	assert f.der['x1'] ==  pytest.approx(1.0)
+	assert f.der['x2'] ==  pytest.approx(1.0)
+	#assert f.val == pytest.approx(7.0)
+
+def test_reverse_sub():
+	x1 = sad.AutoDiffReverse(4, 'x1')
+	x2 = sad.AutoDiffReverse(3, 'x2')
+	
+	f = x1 - 1
+	assert f.der['x1'] ==  pytest.approx(1.0)
+	#assert f.val == pytest.approx(3.0)
+
+	f = 1 - x1
+	assert f.der['x1'] ==  pytest.approx(-1.0)
+	#assert f.val == pytest.approx(-3.0)
+
+	f = x1 - x2
+	assert f.der['x1'] ==  pytest.approx(1.0)
+	assert f.der['x2'] ==  pytest.approx(-1.0)
+	#assert f.val == pytest.approx(1.0)
+
+def test_reverse_mul():
+	x1 = sad.AutoDiffReverse(4, 'x1')
+	x2 = sad.AutoDiffReverse(3, 'x2')
+	
+	f = x1 * 2
+	assert f.der['x1'] ==  pytest.approx(2.0)
+	#assert f.val == pytest.approx(8.0)
+
+	f = 2 * x1
+	assert f.der['x1'] ==  pytest.approx(2.0)
+	#assert f.val == pytest.approx(8.0)
+
+	f = x1 * x2
+	assert f.der['x1'] ==  pytest.approx(3.0)
+	assert f.der['x2'] ==  pytest.approx(4.0)
+	#assert f.val == pytest.approx(12.0)
+
+
+def test_reverse_neg():
+	x1 = sad.AutoDiffReverse(4, 'x1')
+	
+	f = -x1
+	assert f.der['x1'] ==  pytest.approx(-1.0)
+	#assert f.val == pytest.approx(-4.0)
+
+
+def test_reverse_pow():
+	x1 = sad.AutoDiffReverse(4, 'x1')
+	
+	f = x1 ** 2
+	assert f.der['x1'] ==  pytest.approx(8.0)
+	#assert f.val == pytest.approx(16)
+
+	f = 2 ** x1
+	assert f.der['x1'] ==  pytest.approx(2.0**4*np.log(2))
+	#assert f.val == pytest.approx(2**4)
+
+
+
+def test_reverse_div():
+	x1 = sad.AutoDiffReverse(4, 'x1')
+	x2 = sad.AutoDiffReverse(3, 'x2')
+	
+	f = x1 / 2
+	assert f.der['x1'] ==  pytest.approx(0.5)
+	#assert f.val == pytest.approx(2.0)
+
+	f = 2 / x1
+	assert f.der['x1'] ==  pytest.approx(-2.0/(4**2))
+	#assert f.val == pytest.approx(2/4)
+
+	f = x1 / x2
+	assert f.der['x1'] ==  pytest.approx(1/3)
+	assert f.der['x2'] ==  pytest.approx(-4/(3**2))
+	#assert f.val == pytest.approx(4/3)
+
+if __name__ == '__main__':
+	x1 = sad.AutoDiffReverse(4, 'x1')
+	x2 = sad.AutoDiffReverse(3, 'x2')
+	f = x1 / x2
+	print(sad.forward_pass)
+
+
+
+
+
+
+
+
+
